d33e5978325c892f461418bae20bec92cda65947
msrahaman03 <msrahaman03@gmail.com>
2025-08-29 12:37:21 +0600

    javascript commit

---
+++ index.js
@@ -0,0 +1,113 @@
+const clickHearts =document.getElementById('click-heart')
+clickHearts.addEventListener( 'click',function(){
+          const heartIdSpanTag =document.getElementById('heart-id')
+          const heartIdStr =heartIdSpanTag.innerText
+         
+          let heartId = parseInt(heartIdStr)
+         heartId++
+
+         heartIdSpanTag.innerText = heartId ;
+
+})
+
+
+const clickHeart1 =document.getElementById('click-heart1')
+           clickHeart1.addEventListener( 'click',function(){
+          const heartIdSpanTag =document.getElementById('heart-id')
+          const heartIdStr =heartIdSpanTag.innerText
+         
+          let heartId = parseInt(heartIdStr)
+         heartId++
+
+         heartIdSpanTag.innerText = heartId ;
+
+})
+
+const clickHeart2 =document.getElementById('click-heart2')
+           clickHeart2.addEventListener( 'click',function(){
+          const heartIdSpanTag =document.getElementById('heart-id')
+          const heartIdStr =heartIdSpanTag.innerText
+         
+          let heartId = parseInt(heartIdStr)
+         heartId++
+
+         heartIdSpanTag.innerText = heartId ;
+
+})
+
+const clickHeart3 =document.getElementById('click-heart3')
+           clickHeart3.addEventListener( 'click',function(){
+          const heartIdSpanTag =document.getElementById('heart-id')
+          const heartIdStr =heartIdSpanTag.innerText
+         
+          let heartId = parseInt(heartIdStr)
+         heartId++
+
+         heartIdSpanTag.innerText = heartId ;
+
+})
+
+const clickHeart4 =document.getElementById('click-heart4')
+           clickHeart4.addEventListener( 'click',function(){
+          const heartIdSpanTag =document.getElementById('heart-id')
+          const heartIdStr =heartIdSpanTag.innerText
+         
+          let heartId = parseInt(heartIdStr)
+         heartId++
+
+         heartIdSpanTag.innerText = heartId ;
+
+})
+
+const clickHeart5 =document.getElementById('click-heart5')
+           clickHeart5.addEventListener( 'click',function(){
+          const heartIdSpanTag =document.getElementById('heart-id')
+          const heartIdStr =heartIdSpanTag.innerText
+         
+          let heartId = parseInt(heartIdStr)
+         heartId++
+
+         heartIdSpanTag.innerText = heartId ;
+
+})
+
+const clickHeart6 =document.getElementById('click-heart6')
+           clickHeart6.addEventListener( 'click',function(){
+          const heartIdSpanTag =document.getElementById('heart-id')
+          const heartIdStr =heartIdSpanTag.innerText
+         
+          let heartId = parseInt(heartIdStr)
+         heartId++
+
+         heartIdSpanTag.innerText = heartId ;
+
+})
+
+const clickHeart7 =document.getElementById('click-heart7')
+           clickHeart7.addEventListener( 'click',function(){
+          const heartIdSpanTag =document.getElementById('heart-id')
+          const heartIdStr =heartIdSpanTag.innerText
+         
+          let heartId = parseInt(heartIdStr)
+         heartId++
+
+         heartIdSpanTag.innerText = heartId ;
+
+})
+
+
+const clickHeart8 =document.getElementById('click-heart8')
+           clickHeart8.addEventListener( 'click',function(){
+          const heartIdSpanTag =document.getElementById('heart-id')
+          const heartIdStr =heartIdSpanTag.innerText
+         
+          let heartId = parseInt(heartIdStr)
+         heartId++
+
+         heartIdSpanTag.innerText = heartId ;
+
+})
+
+
+
+
